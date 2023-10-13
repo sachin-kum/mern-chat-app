61182e1fd41028fe602bab0d496f7727ace87599
sachin-kum <125179617+sachin-kum@users.noreply.github.com>
2023-10-13 17:54:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 const userRouters = require("./routes/userRoutes");
 const cloudinary = require("cloudinary");
-const path = require("path");
+// const path = require("path");
 // env.config();
 connectDB();
 app.use(
@@ -31,19 +31,19 @@ app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 
 // --------------------------deployment------------------------------
 
